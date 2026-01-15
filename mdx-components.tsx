8b686106fe702cfbce7138970c567f7de114fe87
Than <thanzero7@gmail.com>
2026-01-15 14:08:34 +0700

    penambahan fitur

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,44 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import { Card, Cards } from 'fumadocs-ui/components/card';
+import {
+  LayoutDashboard,
+  Monitor,
+  Album,
+  Database,
+  Activity,
+  FileText,
+  History,
+  UserCheck,
+  Settings,
+  Wrench,
+  Home,
+  BookOpen,
+  CreditCard,
+  Box,
+  ClipboardList
+} from 'lucide-react';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Card,
+    Cards,
+    LayoutDashboard,
+    Monitor,
+    Album,
+    Database,
+    Activity,
+    FileText,
+    History,
+    UserCheck,
+    Settings,
+    Wrench,
+    Home,
+    BookOpen,
+    CreditCard,
+    Box,
+    ClipboardList,
     ...components,
   };
 }
